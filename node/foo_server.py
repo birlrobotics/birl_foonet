fd4d89dc3d84bff7f30e29dedb06d2ff17060172
Tony <470963660@qq.com>
2018-11-07 11:21:11 +0800

    add new action_lib

--- node/foo_server.py
+++ node/foo_server.py
@@ -1,25 +1,92 @@
 #! /usr/bin/env python
 
 import rospy
-
 import actionlib
-
-import birl_foonet.msg
-
+import numpy as np
 from copy import deepcopy
-import ipdb
+# normal libraries
+import birl_foonet.msg
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose,TransformStamped,Transform
+from baxter_core_msgs.msg import EndpointState
+from tf.transformations import (
+    translation_matrix,
+    quaternion_matrix,
+    translation_from_matrix,
+    quaternion_from_matrix,
+)
+# ros msg
+import ipdb
+# debug signal
 
-from birl_foonet.msg import foonet
+# global variables
+marker_msg = None
+right_endpoint_msg = None
+_rate = 10
+flag_marker = False
+flag_endpoint_state = False
+image_processing_method = "ar_marker"
 
-shared_msg = None
+table_pose = np.array(
+        ((1.0, 0.0, 0.0, -0.0),
+        (0.0, 1.0, 0.0, -0.0),
+        (0.0, 0.0, 1.0, 0.05),
+        (0.0, 0.0, 0.0, 1.0))
+    , dtype=np.float64)
 
-def cb(msg):
-    global shared_msg
-    shared_msg = msg.markers
 
-class FibonacciAction(object):
+def image_flag(msg):
+    global marker_msg,flag_marker
+    if not flag_marker:
+        print "maker signal is ok"
+        flag_marker = True
+    marker_msg = msg.markers
+
+def callback_endpoint_state(data):
+    global flag_endpoint_state,right_endpoint_msg
+    if not flag_endpoint_state:
+        flag_endpoint_state = True
+        print 'endpoint state signals is OK!'
+    right_endpoint_msg = data.pose
+
+def compute_transfom(object_pose,gripper_pose,table_pose):
+    
+    object_mat = np.dot(translation_matrix((object_pose.position.x, object_pose.position.y, object_pose.position.z)), 
+    quaternion_matrix((object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z,object_pose.orientation.w)))
+    
+    endpoint_mat = np.dot(translation_matrix((gripper_pose.position.x, gripper_pose.position.y, gripper_pose.position.z)), 
+    quaternion_matrix((gripper_pose.orientation.x, gripper_pose.orientation.y, gripper_pose.orientation.z,gripper_pose.orientation.w)))
+
+    T_gripper_marker = np.dot(np.linalg.inv(endpoint_mat),object_mat)
+    T_table_marker = np.dot(np.linalg.inv(table_pose),object_mat)
+
+    marker_gripper_trans = translation_from_matrix(T_gripper_marker)
+    marker_gripper_quat = quaternion_from_matrix(T_gripper_marker)
+    marker_gripper_tranform = Transform()
+    marker_gripper_tranform.translation.x = marker_gripper_trans[0]
+    marker_gripper_tranform.translation.y = marker_gripper_trans[1]
+    marker_gripper_tranform.translation.z = marker_gripper_trans[2]
+    marker_gripper_tranform.rotation.x = marker_gripper_quat[0]
+    marker_gripper_tranform.rotation.y = marker_gripper_quat[1]
+    marker_gripper_tranform.rotation.z = marker_gripper_quat[2]
+    marker_gripper_tranform.rotation.w = marker_gripper_quat[3]
+
+    marker_table_trans = translation_from_matrix(T_table_marker)
+    marker_table_quat = quaternion_from_matrix(T_table_marker)
+    marker_table_tranform = Transform()
+    marker_table_tranform.translation.x = marker_table_trans[0]
+    marker_table_tranform.translation.y = marker_table_trans[1]
+    marker_table_tranform.translation.z = marker_table_trans[2]
+    marker_table_tranform.rotation.x = marker_table_quat[0]
+    marker_table_tranform.rotation.y = marker_table_quat[1]
+    marker_table_tranform.rotation.z = marker_table_quat[2]
+    marker_table_tranform.rotation.w = marker_table_quat[3]
+
+    dic = {"marker_gripper":marker_gripper_tranform,
+           "marker_table":marker_table_tranform, }
+    return dic
+
+class FoonetClass(object):
     # create messages that are used to publish feedback/result
     _feedback = birl_foonet.msg.FoonetFeedback()
     _result = birl_foonet.msg.FoonetResult()
@@ -30,46 +97,52 @@ class FibonacciAction(object):
 
     def execute_cb(self, goal):
         
-        r = rospy.Rate(1)
-        success = True
-
+        r = rospy.Rate(_rate)
         
-        
-
+        rospy.sleep(1)
+        # ipdb.set_trace()
         for _obj_idx in goal.object_id:
-            if self._as.is_preempt_requested():
-                rospy.loginfo('%s: Preempted' % self._action_name)
-                self._as.set_preempted()
-                success = False
-                break
-            for msg in shared_msg:
+            for msg in marker_msg:
                 if _obj_idx == msg.id:
-                    # ipdb.set_trace()
-                    foonet_msg = foonet()
-                    foonet_msg.header = msg.header
-                    foonet_msg.pose   = msg.pose.pose
-                    foonet_msg.object_id = msg.id
-                    self._feedback.object_pose.append(foonet_msg)
+                    tem_dic = compute_transfom(msg.pose.pose,right_endpoint_msg,table_pose)
+                    T_object_gripper = TransformStamped()
+                    T_object_gripper.header = msg.header  
+                    T_object_gripper.transform = tem_dic["marker_gripper"]
+                    T_object_gripper.child_frame_id = str(msg.id)
+
+                    T_object_table = TransformStamped()
+                    T_object_table.header = msg.header
+                    T_object_table.transform = tem_dic["marker_table"]
+                    T_object_table.child_frame_id = str(msg.id)
+                    self._feedback.object_gripper_transfrom.append(T_object_gripper)
+                    self._feedback.object_table_transfrom.append(T_object_table)
                     break
             else:
-                self._feedback.missing_id.append(_obj_idx)
+                if _obj_idx not in self._feedback.missing_id:
+                    self._feedback.missing_id.append(_obj_idx)
             self._as.publish_feedback(self._feedback)
-
-        
-            # print self._feedback.object_pose
 
             r.sleep()
             
-        if success:
-            self._result = self._feedback
-            self._as.set_succeeded(self._result)
+        self._result.object_state = self._feedback.missing_id
+        self._as.set_succeeded(self._result)
 
 if __name__ == '__main__':
     rospy.init_node('foonet_server')
-    if rospy.wait_for_message("ar_pose_marker",AlvarMarkers,timeout=5):
-        rospy.Subscriber("ar_pose_marker", AlvarMarkers, cb)
-    else:
-        rospy.logfatal("Could not connect to /ar_pose_marker server, is the server node running?");
-        rospy.signal_shutdown("Required component missing"); 
-    server = FibonacciAction()
+
+    if image_processing_method == "ar_marker":
+        if rospy.wait_for_message("ar_pose_marker",AlvarMarkers,timeout=5):
+            rospy.Subscriber("ar_pose_marker", AlvarMarkers, image_flag)
+        else:
+            rospy.logfatal("Could not connect to /ar_pose_marker server, is the server node running?");
+            rospy.signal_shutdown("Required component missing"); 
+            
+    elif image_processing_method == "baysian_filter":
+        if rospy.wait_for_message("ar_pose_marker",AlvarMarkers,timeout=5):
+            rospy.Subscriber("ar_pose_marker", AlvarMarkers, cb)
+        else:
+            rospy.logfatal("Could not connect to /ar_pose_marker server, is the server node running?");
+            rospy.signal_shutdown("Required component missing"); 
+    rospy.Subscriber("/robot/limb/right/endpoint_state", EndpointState, callback_endpoint_state)
+    server = FoonetClass()
     rospy.spin()
